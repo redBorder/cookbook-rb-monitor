a095e87608d30834f0fe0fcfd8c330f03a9dd19f
Miguel Negron <manegron@redborder.com>
2024-06-08 00:46:43 +0100

    resources

--- resources/resources/config.rb
+++ resources/resources/config.rb
@@ -1,17 +1,16 @@
-# Cookbook Name:: rbmonitor
-#
+# Cookbook:: rbmonitor
 # Resource:: config
-#
+
 actions :add, :remove
 default_action :add
 
-attribute :config_dir, :kind_of => String, :default => "/etc/redborder-monitor"
-attribute :kafka_topic, :kind_of => String, :default => "rb_monitor"
-attribute :name, :kind_of => String, :default => "localhost"
-attribute :hostip, :kind_of => String, :default => "127.0.0.1"
-attribute :community, :kind_of => String, :default => "redborder"
-attribute :log_level, :kind_of => Integer, :default => 3
-attribute :device_nodes, :kind_of => Array, :default => []
-attribute :flow_nodes, :kind_of => Array, :default => []
-attribute :managers, :kind_of => Array, :default => []
-attribute :cluster, :kind_of => Hash, :default => {}
+attribute :config_dir, kind_of: String, default: '/etc/redborder-monitor'
+attribute :kafka_topic, kind_of: String, default: 'rb_monitor'
+attribute :name, kind_of: String, default: 'localhost'
+attribute :hostip, kind_of: String, default: '127.0.0.1'
+attribute :community, kind_of: String, default: 'redborder'
+attribute :log_level, kind_of: Integer, default: 3
+attribute :device_nodes, kind_of: Array, default: []
+attribute :flow_nodes, kind_of: Array, default: []
+attribute :managers, kind_of: Array, default: []
+attribute :cluster, kind_of: Hash, default: {}
