855ffa03449e535a35d9ed49fb4f1739e128b577
Miguel Negron <manegron@redborder.com>
2024-06-08 00:45:56 +0100

    lint attributes

--- resources/attributes/default.rb
+++ resources/attributes/default.rb
@@ -1,9 +1,7 @@
-# default attributes
-
 # default monitor count
-default["redborder"]["monitor"]["count"] = 0
+default['redborder']['monitor']['count'] = 0
 
 # default monitor config
-default["redborder"]["monitor"]["config"] = {}
-default["redborder"]["monitor"]["config"]["sensors"] = []
-default["redborder"]["monitor"]["config"]["conf"] = {}
+default['redborder']['monitor']['config'] = {}
+default['redborder']['monitor']['config']['sensors'] = []
+default['redborder']['monitor']['config']['conf'] = {}
