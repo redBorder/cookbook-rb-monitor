81c1121d0705effd1296fbfb8c3231fd5cc65b1e
vimesa <vimesa@redborder.com>
2025-11-24 08:54:07 +0000

    Fix tests

--- resources/libraries/update_sensors.rb
+++ resources/libraries/update_sensors.rb
@@ -1,20 +1,19 @@
 module Rbmonitor
   module Helpers
-
     NODE_TYPES = {
-      device: ['device_nodes', 'proxy_device_nodes'],
-      flow:   ['flow_nodes',   'proxy_flow_nodes']
+      device: %w[device_nodes proxy_device_nodes],
+      flow:   %w[flow_nodes proxy_flow_nodes],
     }.freeze
 
     # ======================================================
     # Main
     # ======================================================
     def update_sensors(resource)
-      node.default['redborder']['monitor']['config']['sensors'] << "/* Remote sensors */"
+      node.default['redborder']['monitor']['config']['sensors'] << '/* Remote sensors */'
 
       NODE_TYPES.each do |type, (manager_key, proxy_key)|
         # Proxy sensor IDs to filter children
-        proxy_nodes_array = resource["proxy_nodes"] || []
+        proxy_nodes_array = resource['proxy_nodes'] || []
 
         proxy_sensor_ids = proxy_nodes_array.map do |proxy_node|
           proxy_node['redborder'] && proxy_node['redborder']['sensor_id']
@@ -30,15 +29,15 @@ module Rbmonitor
         )
 
         # Proxy sensors
-        if node['redborder']['is_proxy']
-          update_sensor_group(
-            title: "/* #{type.to_s.upcase} SENSORS */",
-            nodes: resource[proxy_key],
-            manager_list: nil,
-            hostname: nil,
-            exclude_parent_ids: nil
-          )
-        end
+        next unless node['redborder']['is_proxy']
+
+        update_sensor_group(
+          title: "/* #{type.to_s.upcase} SENSORS */",
+          nodes: resource[proxy_key],
+          manager_list: nil,
+          hostname: nil,
+          exclude_parent_ids: nil
+        )
       end
     end
 
@@ -57,7 +56,7 @@ module Rbmonitor
         next unless snode['ipaddress']
 
         # Exclude nodes that are children of proxies
-        parent_id = snode.dig('redborder','parent_id')
+        parent_id = snode.dig('redborder', 'parent_id')
         next if exclude_parent_ids&.include?(parent_id)
 
         name  = snode['rbname'] || snode.name
@@ -81,21 +80,20 @@ module Rbmonitor
     # ======================================================
     def build_sensor_hash(snode)
       {
-        'timeout': 5,
-        'sensor_name': snode['rbname'] || snode.name,
-        'sensor_ip': snode['ipaddress'],
-        'community': (snode['redborder']['snmp_community'].to_s.empty? ? 'public' : snode['redborder']['snmp_community'].to_s),
-        'snmp_version': (snode['redborder']['snmp_version'].to_s.empty? ? '2c' : snode['redborder']['snmp_version'].to_s),
-        'snmp_username': snode['redborder']['snmp_username'].to_s,
-        'snmp_security_level': snode['redborder']['snmp_security_level'].to_s,
-        'snmp_auth_protocol': snode['redborder']['snmp_auth_protocol'].to_s,
-        'snmp_auth_password': snode['redborder']['snmp_auth_password'].to_s,
-        'snmp_priv_protocol': snode['redborder']['snmp_priv_protocol'].to_s,
-        'snmp_priv_password': snode['redborder']['snmp_priv_password'].to_s,
-        'enrichment' => enrich(snode),
-        'monitors' => monitors(snode)
+        timeout: 5,
+        sensor_name: snode['rbname'] || snode.name,
+        sensor_ip: snode['ipaddress'],
+        community: (snode['redborder']['snmp_community'].to_s.empty? ? 'public' : snode['redborder']['snmp_community'].to_s),
+        snmp_version: (snode['redborder']['snmp_version'].to_s.empty? ? '2c' : snode['redborder']['snmp_version'].to_s),
+        snmp_username: snode['redborder']['snmp_username'].to_s,
+        snmp_security_level: snode['redborder']['snmp_security_level'].to_s,
+        snmp_auth_protocol: snode['redborder']['snmp_auth_protocol'].to_s,
+        snmp_auth_password: snode['redborder']['snmp_auth_password'].to_s,
+        snmp_priv_protocol: snode['redborder']['snmp_priv_protocol'].to_s,
+        snmp_priv_password: snode['redborder']['snmp_priv_password'].to_s,
+        enrichment: enrich(snode),
+        monitors: monitors(snode),
       }
     end
-
   end
 end
