ffb939a024b9dfc106e0cb11664f78f2472bd48d
Pablo Pérez <pperez@redborder.com>
2026-04-27 10:39:13 +0100

    Fix comment formatting in rbmonitor_helpers.rb

--- resources/libraries/rbmonitor_helpers.rb
+++ resources/libraries/rbmonitor_helpers.rb
@@ -93,7 +93,7 @@ module Rbmonitor
         operation = monitor['system']
         next unless name
 
-        # skip if already inserted with the same operation if has operation or not allowed by data_bag 
+        # skip if already inserted with the same operation if has operation or not allowed by data_bag
         monitors_list = data_bag['monitors']
         next unless (inserted[name].nil? || !inserted_operations.include?(operation)) && (monitors_list.nil? || monitors_list.include?(name))
 
